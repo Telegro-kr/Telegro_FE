9382c3f72fa8df2cd2a61bad4330c1e028e9b60d
Dubabbi <thdms3160@naver.com>
2026-03-27 18:42:16 +0900

    feat: 헤더 수정 (#37)

--- src/shared/layouts/public-header.tsx
+++ src/shared/layouts/public-header.tsx
@@ -6,7 +6,8 @@ const linkClass =
 const PublicHeader = () => {
   return (
     <header className="fixed top-0 z-50 w-full border-b border-[#eee] bg-white px-[3rem] py-[1.6rem]">
-      <nav className="flex gap-[3rem] px-[3rem]">
+      <nav className="flex items-center gap-[3rem] px-[2rem]">
+        <h1 className="title3_bold text-primary pr-1">Telegro</h1>
         <Link to="/" className={linkClass}>
           Home
         </Link>
