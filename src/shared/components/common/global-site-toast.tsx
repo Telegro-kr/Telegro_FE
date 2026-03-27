9fc3bba1a4f54cbad78f5d07372ecd43adfff397
Dubabbi <thdms3160@naver.com>
2026-03-27 18:39:43 +0900

    feat: 공통 헤더 설정 (#37)

--- src/shared/components/common/global-site-toast.tsx
+++ src/shared/components/common/global-site-toast.tsx
@@ -16,11 +16,10 @@ const GlobalSiteToast = () => {
           <Icon name="global-toast" className="text-gray-300" size={2.0} />
         </div>
 
-        <p className="w-[404px] max-w-[calc(100vw-12rem)] text-[14px] leading-[21px] font-medium break-words whitespace-pre-line text-white">
-          본사이트는 기업전용 사이트로 일반고객은 제품구매를 하실 수 없습니다.
-          제품문의 및 구매는 KJ대리점으로 문의하시기 바랍니다.
-          {'\n'}
-          제품출고일 : 매주 목요일(오후3시 접수마감) 입니다.
+        <p className="w-[33rem] max-w-[calc(100vw-12rem)] text-[14px] leading-[21px] font-medium break-words whitespace-pre-line text-white">
+          텔레그로 사이트 방문해주셔서 감사합니다. {'\n'}
+          제품에 대한 문의 주시면 자세하게 설명 드리겠습니다. {'\n'}
+          제품출고마감 : 당일 오후 3시 접수마감 입니다.
         </p>
 
         <button
